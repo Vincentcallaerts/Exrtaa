6027e6a38eed66cfbc5f751cc1657c2000d97dc2
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-22 15:29:18 +0100

    Add project files.

---
+++ Exrtaa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Exrtaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam, voornaam, straat, nummer, stad, postcode;
+            Console.WriteLine("Geef je naam in");
+            naam = Console.ReadLine();
+            Console.WriteLine("Geef je voornaam in");
+            voornaam = Console.ReadLine();
+        }
+    }
+}
