24037f9b20f3051779d88a114ae106dadd2bf56c
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-22 15:36:17 +0100

    Extra oef

--- Exrtaa/Program.cs
+++ Exrtaa/Program.cs
@@ -8,10 +8,23 @@ namespace Exrtaa
         static void Main(string[] args)
         {
             string naam, voornaam, straat, nummer, stad, postcode;
-            Console.WriteLine("Geef je naam in");
+            Console.WriteLine("Geef je naam in: ");
             naam = Console.ReadLine();
-            Console.WriteLine("Geef je voornaam in");
+            Console.WriteLine("Geef je voornaam in: ");
             voornaam = Console.ReadLine();
+            Console.WriteLine("Geef je Straatnaam in: ");
+            straat = Console.ReadLine();
+            Console.WriteLine("Geef je huisnummer in: ");
+            nummer = Console.ReadLine();
+            Console.WriteLine("Geef je stadsnaam in: ");
+            stad = Console.ReadLine();
+            Console.WriteLine("Geef je postcode in: ");
+            postcode = Console.ReadLine();
+
+            Console.WriteLine($"Naam: {voornaam} {naam}");
+            Console.WriteLine($"Straat: {straat} {nummer}, {postcode} {stad}");
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
