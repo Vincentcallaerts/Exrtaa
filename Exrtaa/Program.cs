959f11b15e097e7e25b616b9ca323b7ca03b2523
Vincentcallaerts <57138252+Fattunatunatuna@users.noreply.github.com>
2021-02-23 09:00:00 +0100

    Final stuff

--- Exrtaa/Program.cs
+++ Exrtaa/Program.cs
@@ -8,17 +8,36 @@ namespace Exrtaa
         static void Main(string[] args)
         {
             string naam, voornaam, straat, nummer, stad, postcode;
+
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Geef je naam in: ");
+            Console.ForegroundColor = ConsoleColor.Green;
             naam = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Geef je voornaam in: ");
+            Console.ForegroundColor = ConsoleColor.Green;
             voornaam = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Geef je Straatnaam in: ");
+            Console.ForegroundColor = ConsoleColor.Green;
             straat = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Geef je huisnummer in: ");
+            Console.ForegroundColor = ConsoleColor.Green;
             nummer = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Geef je stadsnaam in: ");
+            Console.ForegroundColor = ConsoleColor.Green;
             stad = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Geef je postcode in: ");
+            Console.ForegroundColor = ConsoleColor.Green;
             postcode = Console.ReadLine();
             
             Console.WriteLine();
